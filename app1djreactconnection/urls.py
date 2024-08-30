aa6ccd46f7c6c304b7a51502f7cdbcc9b9a629d0
Nadeem Parokkot <nadeemparokkot@gmail.com>
2024-08-30 12:17:01 +0530

    added url

--- app1djreactconnection/urls.py
+++ app1djreactconnection/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
-from django.conf.urls import url
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ',ReactView.as_view(),name="anything"),
+    path('',ReactView.as_view(),name="anything")  #this for a class url not a def url
 ]
